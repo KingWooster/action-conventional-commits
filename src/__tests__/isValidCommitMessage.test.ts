cb36793b181369d78b88a42553aebb052ae027f2
John Cashmore <john@cashmo.re>
2024-02-09 09:03:25 +0000

    chore(action): require scope

--- src/__tests__/isValidCommitMessage.test.ts
+++ src/__tests__/isValidCommitMessage.test.ts
@@ -2,12 +2,12 @@ import isValidCommitMessage from "../isValidCommitMesage";
 
 test("should be able to correctly validate the commit message", () => {
     expect(isValidCommitMessage("chore(nice-one): doing this right")).toBe(true);
-    expect(isValidCommitMessage("feat!: change all the things")).toBe(true);
+    expect(isValidCommitMessage("feat!: change all the things")).toBe(false);
     expect(isValidCommitMessage("fix(user)!: a fix with some breaking changes")).toBe(true);
-    expect(isValidCommitMessage("fix: menu must open on shortcut press")).toBe(true);
+    expect(isValidCommitMessage("fix: menu must open on shortcut press")).toBe(false);
     expect(isValidCommitMessage("something: should not work")).toBe(false);
     expect(isValidCommitMessage("fixes something")).toBe(false);
-    expect(isValidCommitMessage("🚧 fix: menu must open on shortcut press")).toBe(true);
+    expect(isValidCommitMessage("🚧 fix: menu must open on shortcut press")).toBe(false);
     expect(isValidCommitMessage("fix(menus): menu must open on shortcut press")).toBe(true);
     expect(isValidCommitMessage("🚧 fix(menus): menu must open on shortcut press")).toBe(true);
     expect(isValidCommitMessage("🚧 fixing something")).toBe(false);
